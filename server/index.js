8c5fc4fee685b558fa6af8bc7b4cd7fdaaa5872d
Mitchell Oliver <Mitchelloliver1991@gmail.com>
2021-12-14 07:08:26 -0800

    Add: dotenv file for URI

--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const ShortUrl = require('./models/shortUrl');
 
 const app = express();
 
-const CONNECTION_URL = "mongodb+srv://Mitch4sho:Pich1234@cluster0.f79uw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+require('dotenv').config();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const CONNECTION_URI = process.env.URI;
 const PORT = process.env.PORT || 9000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => console.log(`server is running on port: ${PORT}`)))
+mongoose.connect(CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => {
+        console.log('mongoDB database connection established')
+        console.log(`server is running on port: ${PORT}`)
+    }))
     .catch((error) => console.log(error.message));
