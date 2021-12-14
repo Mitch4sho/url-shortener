e349adeaace87e5063543aff45461c764bdfdc4d
Mitchell Oliver <Mitchelloliver1991@gmail.com>
2021-12-14 14:15:20 -0800

    Fix: cors issue for request

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const ShortUrl = require('./models/shortUrl.model');
 const shortUrlRouter = require('./routes/shortUrl');
 
@@ -9,13 +10,14 @@ const app = express();
 const CONNECTION_URI = process.env.URI;
 const PORT = process.env.PORT || 9000;
 
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/shortUrl', shortUrlRouter)
 
 mongoose.connect(CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('mongoDB database connection established'))
     .catch((error) => console.log(error.message));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use('/shortUrl', shortUrlRouter)
 
 app.listen(PORT, () => console.log(`server is running on port: ${PORT}`));
