8689c8901712589b1b11de849a4e066c6b92aa64
Mitchell Oliver <Mitchelloliver1991@gmail.com>
2021-12-13 18:32:53 -0800

    Add: connection to the database

--- server/index.js
+++ server/index.js
@@ -1,2 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+
+const app = express();
+
+const CONNECTION_URL = "mongodb+srv://Mitch4sho:Pich1234@cluster0.f79uw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 9000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`server is running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
