88582906981bcfd471a6309eee175db94d46b9f0
Mitchell Oliver <Mitchelloliver1991@gmail.com>
2021-12-14 23:17:09 -0800

    Styles: add styles for Url wrapper

--- client/src/ShortUrl.js
+++ client/src/ShortUrl.js
@@ -1,12 +1,21 @@
 import React from "react";
 import styled from "styled-components";
 
-const ShortUrlWrapper = styled.div``;
+const ShortUrlWrapper = styled.div`
+  padding: 18px;
+  background-color: white;
+  border-radius: 5px;
+  border: 1px solid black;
+
+  a {
+    margin-left: 18px;
+  }
+`;
 
 export default function ShortUrl({ shortenUrl }) {
   return (
     <ShortUrlWrapper>
-      ShortURL
+      Shorten Url:
       <a href={shortenUrl.full} target="_blank" rel="noreferrer">
         {shortenUrl.short}
       </a>
